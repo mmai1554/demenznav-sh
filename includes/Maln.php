5b5c8a26366c9cd59a5251a101e87d71ef44e5cd
Michael Mai <mm@mai-internet.de>
2019-11-04 11:56:57 +0100

    add alink method for drawing a-tags

--- includes/Maln.php
+++ includes/Maln.php
@@ -3,11 +3,10 @@
 namespace mnc;
 
 /**
- * Class MIHTML
+ * Class Maln = Umgangssprache fuer "Zeichne ein ..."
  * HTML components for everday use
  * meant for small reusable staff
  * and as router for Render... Components:
- * @package App\Http\LayoutComponents
  */
 abstract class Maln {
 
@@ -32,7 +31,7 @@ abstract class Maln {
 		$a[] = '<li>';
 		$a[] = '<div class="icon-wrap">';
 		$a[] = '<span class="fl-icon">';
-		$a[] = '<i class="material-icons mnc-iconlist">'.$icon.'</i>';
+		$a[] = '<i class="material-icons mnc-iconlist">' . $icon . '</i>';
 		$a[] = '</span>';
 		$a[] = '<div class="fl-icon-text">';
 		$a[] = $content;
@@ -59,6 +58,32 @@ abstract class Maln {
 			self::id( $id ),
 			self::style( $style ),
 			self::getClass( $class ),
+			self::getDataAttribute( $dataattributes ),
+			$html
+		);
+	}
+
+	/**
+	 * @param string $url
+	 * @param string $html
+	 * @param string $target
+	 * @param string $title
+	 * @param string $class
+	 * @param string $style
+	 * @param string $id
+	 * @param array $dataattributes
+	 *
+	 * @return string
+	 */
+	public static function alink( $url, $html, $target, $title, $class = '', $style = '', $id = '', $dataattributes = [] ) {
+		return sprintf(
+			'<a href="%s"%s%s%s%s%s%s>%s</a>',
+			$url,
+			self::id( $id ),
+			self::target( $target ),
+			self::title( $title ),
+			self::getClass( $class ),
+			self::style( $style ),
 			self::getDataAttribute( $dataattributes ),
 			$html
 		);
@@ -141,8 +166,20 @@ abstract class Maln {
 		return strlen( $id ) ? ' id="' . $id . '"' : '';
 	}
 
+	private static function target( $target ) {
+		if ( $target == '_new' ) {
+			$target = '_blank';
+		}
+
+		return strlen( $target ) ? ' target="' . $target . '"' : '';
+	}
+
 	private static function style( $style = '' ) {
 		return strlen( $style ) ? ' style="' . $style . '"' : '';
+	}
+
+	private static function title( $title = '' ) {
+		return strlen( $title ) ? ' title="' . $title . '"' : '';
 	}
 
 	private static function getClass( $class = '' ) {
